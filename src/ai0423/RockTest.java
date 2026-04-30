fbc1ae4d61390521ae0d34996cbb501383435c1c
woosik980201 <ratel9802@gmail.com>
2026-04-30 09:49:04 +0900

    0430javaSwitch

--- src/ai0423/RockTest.java
+++ src/ai0423/RockTest.java
@@ -10,7 +10,7 @@ public class RockTest {
 
         String[] computer = {"가위","바위","보"};
         Random random =new Random();
-        int randNum = random.nextInt(3)-1;
+        int randNum = random.nextInt(3);
         String computerChoice = computer[randNum];
         System.out.println("* 컴퓨터의 가위, 바위, 보 : " + computer[randNum]);
 
