4a501670941d18a2d8a98a468df53c98f9645731
woosik980201 <ratel9802@gmail.com>
2026-05-28 11:23:16 +0900

    0528-1

--- src/ai0521/rabbit/Rabbit.java
+++ src/ai0521/rabbit/Rabbit.java
@@ -1,23 +1,19 @@
 package ai0521.rabbit;
 
+
 public class Rabbit {
-    private String shape;//토끼의 모양
-    private String xPos;//x 위치
-    private int yPos;//y 위치
 
+    private String shape;
+    private int xPos;
+    private int yPos;
 
-
-
-    //기본 생성자
     public Rabbit() {
     }
-    //일반 생성자
+
     public Rabbit(String shape) {
         this.shape = shape;
     }
 
-    //getter 메소드: 필드에 저장된 값을 반환
-    //setter 메소드: 필드에 값을 설정(저장)
     public String getShape() {
         return shape;
     }
@@ -26,19 +22,41 @@ public class Rabbit {
         this.shape = shape;
     }
 
-    public String getxPos() {
+    public int getXPos() {
         return xPos;
     }
 
-    public void setxPos(String xPos) {
+    public void setXPos(int xPos) {
         this.xPos = xPos;
     }
 
-    public int getyPos() {
+    public int getYPos() {
         return yPos;
     }
 
-    public void setyPos(int yPos) {
+    public void setYPos(int yPos) {
         this.yPos = yPos;
+    }
+
+    public void setLocation(int xPos, int yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    public void printInfo() {
+        System.out.printf("%s 모양의 토끼는 (%d, %d) 좌표에 위치하고 있다.\n",
+                shape, xPos, yPos);
+
+    }
+    //x축으로 10씩 오른쪽으로 이동하는 메소드
+    public void moveRightTen() {
+        xPos += 10;
+    }
+
+
+
+    //x축으로 10씩 왼쪽으로 이동하는 메소드
+    public void moveLeftTen() {
+        xPos -= 10;
     }
 }
---
+++ src/ai0521/rabbit/RunRabbit.java
@@ -0,0 +1,33 @@
+package ai0521.rabbit;
+
+public class RunRabbit {
+
+    public static void main(String[] args) {
+
+        Rabbit rabbit1 = new Rabbit();
+        Rabbit rabbit2 = new Rabbit("작은 네모");
+
+        rabbit1.setLocation(50, 50);
+        rabbit2.setLocation(150, 70);
+
+        rabbit1.setShape("기본");
+        rabbit1.printInfo();
+        rabbit2.printInfo();
+
+        rabbit1.setShape("다이아몬드");
+        rabbit1.setXPos(rabbit1.getXPos() + 60);
+        rabbit1.setYPos(rabbit1.getYPos() + 20);
+        rabbit1.printInfo();
+
+     //좁은 네모 모양의 토끼가 현재 위치에서 오른쪽 50만큼, 왼쪽 20만큼 이동
+        rabbit2.printInfo();//좁은 네모 현재위치 출력
+        for(int i=0;i<5;i++){
+            rabbit2.moveRightTen();//10씩 5번 오른쪽 이동
+        }
+        rabbit2.printInfo();//좁은 네모 현재위치 출력
+        for(int i=0;i<2;i++){
+            rabbit2.moveLeftTen();//10씩 2번 왼쪽 이동
+        }
+        rabbit2.printInfo();
+    }
+}
