f85c09fa2f2be9a3b06c6587806366b93c8fabcf
woosik980201 <ratel9802@gmail.com>
2026-05-28 11:47:21 +0900

    0528-1

---
+++ src/ai0528/access/Rabbit.java
@@ -0,0 +1,29 @@
+package ai0528.access;
+
+public class Rabbit {
+ //   private String shape;
+  //  private int x;
+ //   private int y;
+    public String shape;
+    public int x;
+    public int y;
+// default(생략) 제한자는 같은 패키지 내에서만 접근이 가능하다
+ //   String shape;
+   // int x;
+   // int y;
+
+  //  public String getShape() {
+    //    return shape;
+    //}
+
+    //public void setShape(String shape) {
+      //  this.shape = shape;
+    //}
+    //public void setLocation(int x, int y){
+      //  this.x = x;
+        //this.y = y;
+    //}
+    public void printInfo(){
+        System.out.printf("%s모양의 토끼는 (%d,%d)좌표에 있음.\n",shape,x,y);
+    }
+}
